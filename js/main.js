3292e48538a5288e107b31a8479cd531fa4a14f7
kerlinFranco <kerlinfranco@gmail.com>
2023-12-10 10:37:01 -0300

    actualización

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-s
               <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">CRUD</a>
               <div class="dropdown-menu" aria-labelledby="dropdownId">
                   <a class="dropdown-item" href="productos.html">Productos</a>
-                  <a class="dropdown-item" href="#">Action 2</a>
+                  <a class="dropdown-item" href="clientesCRUD.html">Clientes</a>
               </div>
           </li>
       </ul>
